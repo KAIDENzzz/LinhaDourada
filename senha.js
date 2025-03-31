fcabfcaafb0cb2f4c7115e039bf39d2115436f0b
KAIDENzzz <111551906+KAIDENzzz@users.noreply.github.com>
2025-03-31 01:59:03 -0300

    Update senha.js
    mudança na hash

--- senha.js
+++ senha.js
@@ -8,7 +8,7 @@ async function gerarHash(texto) {
 }
 
 async function validarSenha() {
-    const senhaCorretaHash = "e3c9b89a77eabf7a6ad6b7d3c8f96b1d4b6e76e1fc47c5c76e27b8aabf1670f8"; // Hash da senha
+    const senhaCorretaHash = "575a30bd2975cd02639884b04cc27006ac46473358f0d2f1380cd88abda303ba"; // Hash da senha
     const senhaDigitada = [...Array(4).keys()].map(i => document.getElementById(`d${i + 1}`).value).join("");
 
     if (senhaDigitada.length === 4) {
